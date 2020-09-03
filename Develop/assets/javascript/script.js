c443c8fa32d16824000cc1fb27ca796007be508b
Ed Sivick <ed@sivick.net>
2020-09-03 12:47:23 -0400

    question for BCS TA

--- Develop/assets/javascript/script.js
+++ Develop/assets/javascript/script.js
@@ -1,41 +1,41 @@
 // Add event listener to Start Quiz button
 var startQuiz = document.querySelector(".startBtn");
-var question = document.querySelector("span");
-var answer1 = document.querySelector(".a1");
-var answer2 = document.querySelector(".a2");
-var answer3 = document.querySelector(".a3");
-var answer4 = document.querySelector(".a4");
-var checkBox1 = document.getElementById("#flexCheckDefault-1");
+var questionView = document.querySelector("span");
+var answer1View = document.querySelector(".a1");
+var answer2View = document.querySelector(".a2");
+var answer3View = document.querySelector(".a3");
+var answer4View = document.querySelector(".a4");
+var checkBox1 = document.getElementById("flexCheckDefault-1");
+var checkBox2 = document.getElementById("flexCheckDefault-2");
+var checkBox3 = document.getElementById("flexCheckDefault-3");
+var checkBox4 = document.getElementById("flexCheckDefault-4");
 // Find out if checkBox1 is checked
-// var stateOfBox1 = checkBox1.checked;
+var stateOfBox1 = checkBox1.checked;
 
-startQuiz.addEventListener("click", askQuestion);
+startQuiz.addEventListener("click", startQuiz);
 
-var questionArray = [];
-var answer1Array = [];
-var correctAnswerArray = [];
-
-questionArray = ["(1): Which is a correct IF statement if 'x' is NOT equal to 10?"];
-
-answer1Array = ["if (x <> 10)", "if (x =! 10)", "if (x not= 10)", "if (x != 10)"];
-
-correctAnswerArray = ["if (x != 10)"];
-
-var numQuestions = questionArray.length;
-
-function askQuestion() {
-  
-  for (var i = 0; i < questionArray.length; i++) {
-    question.textContent = questionArray[i];
-    answer1.textContent = answer1Array[i];
-    answer2.textContent = answer1Array[i + 1];
-    answer3.textContent = answer1Array[i + 2];
-    answer4.textContent = answer1Array[i + 3];
-    
-    console.log(question);
-    console.log(answer1);
-    console.log(checkBox1);
-    // console.log(stateOfBox1);
-  }
+function startQuiz() {
+  askQuestion()
 }
-  
+
+var questions = [{
+  question: "(1): Which is a correct IF statement if 'x' is NOT equal to 10?",
+  choices: ["if (x <> 10)", "if (x =! 10)", "if (x not= 10)", "if (x != 10)",],
+  answer: 4,
+}]
+
+function askQuestion(question) {
+
+  questionView.textContent = question.question;
+  answer1View.textContent = question.choices[0];
+  answer2View.textContent = question.choices[1];
+  answer3View.textContent = question.choices[2];
+  answer4View.textContent = question.choices[3];
+
+  console.log(questionView);
+  console.log(answer1View);
+  console.log(answer2View);
+  console.log(answer3View);
+  console.log(answer4View);
+  console.log(stateOfBox1);
+}
